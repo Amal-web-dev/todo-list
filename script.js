821705993644e0d1efcd1f8a45606cbaa5857c62
Amal <amalabdurahmonov427@gmail.com>
2023-05-11 12:21:27 +0300

    second

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let input = document.querySelector('.input')
 let cont = document.querySelector('.container')
 let btnAdd = document.querySelector('.add')
 let todos = []
+let trush = document.querySelector('.trush')
 
 form.onsubmit = (event) => {
    event.preventDefault()
@@ -29,35 +30,52 @@ form.onsubmit = (event) => {
    let card = document.createElement('div')
    let pGoal = document.createElement('p')
    let pTime = document.createElement('p')
+   let imgClose = document.createElement('img')
+   let btnDone = document.createElement('button')
+   let done = document.createElement('p')
 
    card.classList.add('card')
    pTime.classList.add('time')
+   imgClose.classList.add('img-close')
+
    pTime.innerHTML = task.time
    pGoal.innerHTML = input.value
+   btnDone.innerHTML = 'Done'
+   btnDone.classList.add('btn-done')
+   imgClose.src = './icon/211651_close_round_icon.svg'
    input.setAttribute('name', 'task')
+
    cardBlock.append(card)
-   card.append(pGoal, pTime)
+   card.append(pGoal, pTime, imgClose, btnDone, done)  
+
+   btnDone.onclick = () => {
+      pGoal.classList.add('text')
+      done.classList.add('done')
+      done.innerHTML = 'Done in ' + new Date().getHours() + ':' + new Date().getMinutes()
+   }
+
+   imgClose.onclick = () => {
+      card.remove()
+      todos.splice(todos.length - 1, 1)
+      todos = []
+      console.log(todos)
+   }
+
+   trush.onclick = () => {
+      const cards = document.querySelectorAll('.card')
+      cards.forEach(card => {
+      //   card.classList.add('trush-bum')
+      //   setTimeout(() => {
+          card.remove()
+      //   }, 3000)
+      })
+      todos = []
+      console.log(todos)
+   }
+
 
    todos.push(task)
-   // reload(todos, )
 
-   console.log(todos);
+   console.log(todos)
    form.reset()
-}
-//  else {
-//    form.onsubmit = (event) => {
-//       event.preventDefault()
-   
-   
-//       form.reset()
-   
-//    }
-// }
-
-function reload(arr, place) {
-   place.innerHTML = ''
-
-   for(let item of arr) {
-       
-   }
 }
